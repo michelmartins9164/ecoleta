82b96a6ba75d13fab7915329d399b753911f13d3
michelmartins9164 <michelmartins9164@gmail.com>
2020-07-29 17:05:06 -0300

    inicialization the project

---
+++ scripts/intro.js
@@ -0,0 +1,57 @@
+// coments
+// document.write('hello')
+
+// variaveis, tipo de dados
+// var myvar = "hello"
+// const myconst = ('hi')
+
+// document.write(myconst + myvar)
+
+
+//string
+// "isso é uma string"
+// 'isso é uma string'
+// `isso é uma string`
+
+//bolean
+// const bTrue = true
+// const bFalse = false
+// document.write(bFalse)
+
+//object possuem propriedades e funcionalidade
+// const pessoa = {
+//   altura: "1,80m",
+//   idade: "24",
+//   solteiro: "true",
+//   correr(){
+//     document.write("uma grande corrida")
+//   }
+// }
+
+// Array Vetores
+// const colecaoDeBolinhas = ["blues", "red", 1,{name: "My name"}]
+// document.write(colecaoDeBolinhas[3].name) 
+
+// Funções - Funcionalidades - Atalhos - Reuso de código
+
+//Registrar função
+// function sayMyName(name) {
+//   document.write(name)
+// }
+// //Executar
+// sayMyName("luana")
+// sayMyName("michel")
+
+//condicionais
+// const notaFinal = 6
+// if(notaFinal < 5) {
+//   document.write ("Aprovado")
+// } else {
+//   document.write("Reprovado")
+// }
+
+//loop repetições
+
+// for (i = 0; i < 5; i++){
+//   document.write(`<p>${i}</p>`)
+// }
