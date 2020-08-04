10a06bc4634a28a936fe92c85400ec80d290fa9d
michelmartins9164 <michelmartins9164@gmail.com>
2020-08-04 14:15:06 -0300

    adding template engine

--- src/server.js
+++ src/server.js
@@ -4,16 +4,33 @@ const server = express()
 
 // configurar pasta pública 
 server.use(express.static("public"))
+
+
+
+const nunjucks = require("nunjucks")
+nunjucks.configure("src/views", {
+    express: server,
+    noCache: true
+})
+
+
+
+
+
 // configurar caminhos 
 //página inicial
 // req: pedido,requisição
 //res: resposta
 server.get("/", (req, res) => {
-    res.sendFile(__dirname + "/views/index.html")
+  return res.render("index.html", {title: "um titulo"})
 })
 
 server.get("/create-point", (req, res) => {
-    res.sendFile(__dirname + "/views/create-point.html")
+   return res.render("create-point.html")
+})
+
+server.get("/search", (req, res) => {
+   return res.render("search-results.html")
 })
 
 //Ligar o servidor
